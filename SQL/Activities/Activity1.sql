74b62fd716ccc0bda4d6d995c1f05ea4531b7eb7
UmasreeP <39192421+UmasreeP@users.noreply.github.com>
2022-03-16 12:55:35 +0530

    Add files via upload

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,9 @@
+-- Create salesman table
+CREATE TABLE salesman (
+    salesman_id int,
+    salesman_name varchar2(20),
+    salesman_city varchar2(20),
+    commission int
+);
+
+DESCRIBE salesman;
